b863d38d0c9e4c7e17c747bfb77ced20b8c476b0
Kallfaktorn <mattiaspgames@gmail.com>
2012-04-23 18:02:12 -0500

    Added terrain types

--- src/com/teatime/game/model/Province.java
+++ src/com/teatime/game/model/Province.java
@@ -2,6 +2,14 @@ package com.teatime.game.model;
 
 public class Province {
 	
+	private enum Terrain { grass, stone, water, wood }
+	
+	private enum Grass { meadow, steppe }
+	private enum GrassWater { swamp }
+	private enum Stone { rocky, mountain }
+	private enum Water { lake, river, sea }
+	private enum Wood { grove, forrest }
+	
 	private Animals animals;
 
 }
